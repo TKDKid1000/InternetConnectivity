2c186a37a95d23134dbb8f8f1a7bfd21c128bf66
TKDKid1000 <rhoneeiler@gmail.com>
2021-03-07 10:27:23 -0800

    logged all errors to a file

--- code/src/io/github/tkdkid1000/Main.java
+++ code/src/io/github/tkdkid1000/Main.java
@@ -1,11 +1,28 @@
 package io.github.tkdkid1000;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Main {
 
 	public static void main(String[] args) {
+		try {
+			System.setErr(new PrintStream(new FileOutputStream("error.log")));
+		} catch (FileNotFoundException e) {
+			try {
+				FileWriter fw = new FileWriter("error.log");
+				fw.write("create");
+				fw.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
 		Timer timer = new Timer();
 		Commands commands = new Commands();
 		timer.scheduleAtFixedRate(new TimerTask() {
